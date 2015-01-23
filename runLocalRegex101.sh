0769622cff37fb9f8c90c5b38a3763190123ec5a
Cody Taylor <cody.taylor@maternityneighborhood.com>
2015-01-22 20:14:37 -0500

    One time local server.
    This script uses the directions from README.md
    to create a one-time clone of regex101.com.

---
+++ runLocalRegex101.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+trap ctrl_c SIGINT;
+
+tempdir=`mktemp -d`
+
+pushd $tempdir
+
+wget -r --no-host-directories --no-parent http://regex101.com
+wget --output-document ./js/javascript.regex101.js http://regex101.com/js/javascript.regex101.js;
+wget --output-document ./js/pcre.regex101.js http://regex101.com/js/pcre.regex101.js;
+wget --output-document ./js/pcrelib16.js http://regex101.com/js/pcrelib16.js;
+
+function server {
+ python3 -m http.server $@;
+}
+
+function cleanup {
+ echo "Cleaning up downloaded files...";
+ popd;
+ rm -rf ${tempdir};
+}
+
+function ctrl_c {
+ cleanup;
+ exit $?;
+}
+
+server $@;
