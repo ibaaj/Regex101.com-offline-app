edac13707cd8dbdebafd3c46dfe30a65ad051f24
x0x8x <5467132+x0x8x@users.noreply.github.com>
2019-07-30 16:47:18 +0300

    Update runLocalRegex101.sh
    JS bundle URL updates

--- runLocalRegex101.sh
+++ runLocalRegex101.sh
@@ -7,8 +7,14 @@ tempdir=`mktemp -d`
 pushd $tempdir
 
 wget -r --no-host-directories --no-parent https://regex101.com;
-wget --output-document ./pcreWorker.js https://regex101.com/pcreWorker.js;
-wget --output-document ./pcrelib.js https://regex101.com/pcrelib.js;
+wget --output-document ./static/golangWorker.js https://regex101.com/static/golangWorker.js;
+wget --output-document ./static/pcreWorker.js https://regex101.com/static/pcreWorker.js;
+wget --output-document ./static/javascriptWorker.js https://regex101.com/static/javascriptWorker.js;
+wget --output-document ./static/pcrelib.js https://regex101.com/static/pcrelib.js;
+wget --output-document ./static/golang.js https://regex101.com/static/golang.js;
+wget --output-document ./static/bundle.js https://regex101.com/static/bundle.js;
+wget --output-document ./static/vendor.71600b0e.chunk.js https://regex101.com/static/vendor.71600b0e.chunk.js;
+wget --output-document ./static/sw.js https://regex101.com/static/sw.js;
 
 function server {
   python -m SimpleHTTPServer $@;
